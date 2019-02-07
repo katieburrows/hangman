bb2ef3ee6025f2b33d515259af7c74edf8331ebc
katieburrows <katie_burrows@me.com>
2019-02-06 17:28:16 -0700

    changed logic of game to create a p container for each letter when looped through then added data- attribute to each new p

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordArray = ["volcano", "earthquake", "tsunami", "hurricane", "tornado", "flood"];
+var wordArray = ["volcano", "earthquake", "tsunami", "hurricane", "tornado", "flood", "fire"];
 
 
 var wins = 0;
@@ -12,26 +12,41 @@ function initializeApp (){
     var originalPicture = "";
     var text = "";
     var targetDiv = "";
+    var letterDash = "";
 
+    var targetDiv = document.getElementById("letterDisplay");
 
 
     currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
     
     currentWord = currentWord.split("");
+
+    console.log(currentWord);
+
     for (var i = 0; i < currentWord.length; i++){
-        var targetDiv = document.getElementById("letterDisplay");
-        text = targetDiv.text = "_ ";
-        targetDiv.append(text);
+        var letterContainer = document.createElement("p");
+        
+        letterContainer.setAttribute("data-letter", currentWord[i]);
+        
+        letterContainer.textContent = "_ ";
+        
+        targetDiv.appendChild(letterContainer);
+
     }
     
-    console.log(currentWord);
+   
 
 //the scoreboard is set to 0.
         
 //To start(can use initializeApp() for everything except the scoreboard):  
     //load the page with a randomly selected word from the wordArray.
         //split the word up into the correct number of underscores.
+        //make each letter into it's own div
+            //each new div needs a data- attr to be set equal to the letter that it represents.
+                // var d = document.getElementById("test");  
+                // d.setAttribute('data-id' , 'Next'); 
             //then push the word onto the page as blank underscores.
+
     //the guesses counter is set to 10.
     //the letters guessed section is empty.
     //display the game's theme picture.
@@ -44,18 +59,31 @@ function initializeApp (){
             if(userKeyPress === currentWord[i]){
                 // userKeyPress.push(lettersGuessedArray);
 
-                text = (text.text = userKeyPress);
-                console.log(`${"text pickup test: "}${text}`)
-                targetDiv.append(text);
-            
+               
+
+                text = (targetDiv.text = userKeyPress);
+                targetDiv.innerHTML = text;
+                
+
+                
             } 
             
         }
         
     }
+
+    //indexOf the user's key press as a var, then (replace method if around) .text(userKeyPress)
+    //a.forEach(function(item, i) { if (item == 3452) a[i] = 1010; });
+    // a = a.map(function(item) { return item == 3452 ? 1010 : item; });
+
+
+
+
+
+
 //collect the user's keypress
     //compare the keypress with the letters of the word
-        //if the letter matches any part of the word display that letter in the dashes.
+        //if the letter matches any part of the word display overwrite the dashes with that letter.
             //add the letter to an array of letters that have been guessed.
         //if the letter doesn't match any part of the word push it to the letters guessed section
             //add the letter to an array of letters that have been guessed.
